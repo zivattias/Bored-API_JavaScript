894a75ea581bce1e3a7ed80b827ca02ffcdef1e0
zivattias <ziv.attias7@gmail.com>
2023-02-10 21:42:00 +0200

    added js functionality partially

--- src/scripts/api.js
+++ src/scripts/api.js
@@ -1,12 +1,60 @@
-// Enable Bootstrap's tooltip utility:
-$(function () {
-    $('[data-toggle="tooltip"]').tooltip()
+const API_URL = "http://www.boredapi.com/api/activity/"
+
+// Enable Bootstrap's tooltip utility - currently unneeded:
+// $(function () {
+//     $('[data-toggle="tooltip"]').tooltip()
+// })
+
+// Adding an eventListener for window to finish loading, then run DOM-manipulating functions:
+window.addEventListener("load", () => {
+    changeParticipantsNumber();
+    isFreeCheckboxHandler();
 })
 
-const rangeInput = document.getElementById("customRange2");
-const rangeHandle = document.querySelector(".range-handle");
+// Change number of participants on range input:
+function changeParticipantsNumber() {
+    const participantsForm = document.getElementById("apiForm")
+    const participantsNumber = document.getElementById("numParNumber")
+    const participantsRange = document.getElementById("numPar")
 
-rangeInput.addEventListener("input", function () {
-    const rangeValue = rangeInput.value;
-    rangeHandle.setAttribute("title", rangeValue);
-});
+    participantsRange.addEventListener("input", () => {
+        participantsNumber.innerHTML = participantsRange.value
+    })
+    // Added an eventListener to the form to prevent span-text bug (showed previous # of participants despite 'reset'):
+    participantsForm.addEventListener("reset", () => {
+        participantsNumber.innerHTML = "1";
+        participantsRange.value = "1";
+    })
+}
+
+function isFreeCheckboxHandler() {
+    const checkbox = document.getElementById("isFree");
+    checkbox.addEventListener("change", () => {
+        if (checkbox.checked) {
+            checkbox.value = "0.0"
+        } else {
+            checkbox.value = ""
+        }
+    })
+    // Second if-else option (one-liner using the ternary operator:
+    // const checkbox = document.getElementById("isFree");
+    // checkbox.addEventListener("change", () => {
+    //     checkbox.value = checkbox.checked ? "0.0" : "";
+    // })
+}
+
+
+// Form submit handler & Fetch API:
+function formSubmitHandler(event) {
+    event.preventDefault()
+    fd = new FormData(event.target)
+    console.log(fd)
+    for (const pair of fd.entries()) {
+        // Bored API query param for 'type' should be lowercased:
+        if (pair[0] === 'type') {
+            pair[1] = pair[1].toLowerCase()
+        }
+        console.log(pair)
+    }
+
+}
